5ae54d2916918dc5a9c1a5dcf2d1bbe1317345d1
FacelessLaw <and_rei99@mail.ru>
2021-12-23 02:12:57 +0300

    Third stage

--- linearization.py
+++ linearization.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy.optimize import linprog
 from matplotlib import pyplot as plt
 
@@ -54,25 +55,312 @@ def linear_minimize(f, x0, g_list, eps=1e-4, dx=1e-5,
         x = res.x
         
         maxstep *= step_decrement
-        print(x0)
-        print(x)
-        print(np.linalg.norm(x - x0))
-        print(eps)
-        print("\n")
+        # print(x0)
+        # print(x)
+        # print(np.linalg.norm(x - x0))
+        # print(eps)
+        # print("\n")
         
     return x, niter
 
 if __name__ == '__main__':
     f = lambda x: 100*((x[1:] - x[0])**2).sum() + (x[0] - 4)**2
     g1 = lambda x: sum([(i+1)*v**2 for i, v in enumerate(x)]) - 79
+    
     x0 = [3, 2, 4, 2.5, 3.1]
 
-    niter_all = []
-    eps_all = np.linspace(0.01, 0.1, num=10)
-    for eps in eps_all:
-        x_min, niter = linear_minimize(f, x0, [g1], maxstep=10, step_decrement=0.99, eps=eps)
-        niter_all.append(niter)
+    # niter_all = []
+    # eps_all = np.linspace(0.01, 0.1, num=10)
+    # for eps in eps_all:
+    #     x_min, niter = linear_minimize(f, x0, [g1], maxstep=10, step_decrement=0.99, eps=eps)
+    #     niter_all.append(niter)
         
-    plt.style.use('ggplot')
-    plt.plot(eps_all, niter_all)
-    plt.savefig('plot.png')
+    # plt.style.use('ggplot')
+    # plt.plot(eps_all, niter_all)
+    # plt.savefig('plot.png')
+
+    curr_eps = 1e-4
+    step = 1e-1
+    t1 = {
+        "x0_1": [],
+        "x0_2": [],
+        "x0_3": [],
+        "x0_4": [],
+        "x0_5": [],
+        "iterations": [],
+        "xmin_1": [],
+        "xmin_2": [],
+        "xmin_3": [],
+        "xmin_4": [],
+        "xmin_5": [],
+        "f(xmin)": [],
+        "||x0-xmin||": []
+    }
+    for i in range(0, 101):
+        x1 = -10 + step * i
+        old_x0_1 = x0[0]
+        x0[0] = x1
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t1["x0_1"].append(x0[0])
+        t1["x0_2"].append(x0[1])
+        t1["x0_3"].append(x0[2])
+        t1["x0_4"].append(x0[3])
+        t1["x0_5"].append(x0[4])
+        t1["iterations"].append(niter)
+        t1["xmin_1"].append(x_min[0])
+        t1["xmin_2"].append(x_min[1])
+        t1["xmin_3"].append(x_min[2])
+        t1["xmin_4"].append(x_min[3])
+        t1["xmin_5"].append(x_min[4])
+        t1["f(xmin)"].append(f(x_min))
+        t1["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[0] = old_x0_1
+    
+    for i in range(0, 101):
+        x2 = -10 + step * i
+        old_x0_2 = x0[1]
+        x0[1] = x2
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t1["x0_1"].append(x0[0])
+        t1["x0_2"].append(x0[1])
+        t1["x0_3"].append(x0[2])
+        t1["x0_4"].append(x0[3])
+        t1["x0_5"].append(x0[4])
+        t1["iterations"].append(niter)
+        t1["xmin_1"].append(x_min[0])
+        t1["xmin_2"].append(x_min[1])
+        t1["xmin_3"].append(x_min[2])
+        t1["xmin_4"].append(x_min[3])
+        t1["xmin_5"].append(x_min[4])
+        t1["f(xmin)"].append(f(x_min))
+        t1["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[1] = old_x0_2
+    
+    for i in range(0, 101):
+        x3 = -10 + step * i
+        old_x0_3 = x0[2]
+        x0[2] = x3
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t1["x0_1"].append(x0[0])
+        t1["x0_2"].append(x0[1])
+        t1["x0_3"].append(x0[2])
+        t1["x0_4"].append(x0[3])
+        t1["x0_5"].append(x0[4])
+        t1["iterations"].append(niter)
+        t1["xmin_1"].append(x_min[0])
+        t1["xmin_2"].append(x_min[1])
+        t1["xmin_3"].append(x_min[2])
+        t1["xmin_4"].append(x_min[3])
+        t1["xmin_5"].append(x_min[4])
+        t1["f(xmin)"].append(f(x_min))
+        t1["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[2] = old_x0_3
+    
+    for i in range(0, 101):
+        x4 = -10 + step * i
+        old_x0_4 = x0[3]
+        x0[3] = x4
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t1["x0_1"].append(x0[0])
+        t1["x0_2"].append(x0[1])
+        t1["x0_3"].append(x0[2])
+        t1["x0_4"].append(x0[3])
+        t1["x0_5"].append(x0[4])
+        t1["iterations"].append(niter)
+        t1["xmin_1"].append(x_min[0])
+        t1["xmin_2"].append(x_min[1])
+        t1["xmin_3"].append(x_min[2])
+        t1["xmin_4"].append(x_min[3])
+        t1["xmin_5"].append(x_min[4])
+        t1["f(xmin)"].append(f(x_min))
+        t1["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[3] = old_x0_4
+    
+    for i in range(0, 101):
+        x5 = -10 + step * i
+        old_x0_5 = x0[4]
+        x0[4] = x5
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t1["x0_1"].append(x0[0])
+        t1["x0_2"].append(x0[1])
+        t1["x0_3"].append(x0[2])
+        t1["x0_4"].append(x0[3])
+        t1["x0_5"].append(x0[4])
+        t1["iterations"].append(niter)
+        t1["xmin_1"].append(x_min[0])
+        t1["xmin_2"].append(x_min[1])
+        t1["xmin_3"].append(x_min[2])
+        t1["xmin_4"].append(x_min[3])
+        t1["xmin_5"].append(x_min[4])
+        t1["f(xmin)"].append(f(x_min))
+        t1["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[4] = old_x0_5
+    table1 = pd.DataFrame(t1)
+    table1.to_excel('./table1.xlsx')
+
+    step = 1
+    t2 = {
+        "x0_1": [],
+        "x0_2": [],
+        "x0_3": [],
+        "x0_4": [],
+        "x0_5": [],
+        "iterations": [],
+        "xmin_1": [],
+        "xmin_2": [],
+        "xmin_3": [],
+        "xmin_4": [],
+        "xmin_5": [],
+        "f(xmin)": [],
+        "||x0-xmin||": [],
+    }
+    for i in range(0, 101):
+        x1 = -50 + step * i
+        old_x0_1 = x0[0]
+        x0[0] = x1
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t2["x0_1"].append(x0[0])
+        t2["x0_2"].append(x0[1])
+        t2["x0_3"].append(x0[2])
+        t2["x0_4"].append(x0[3])
+        t2["x0_5"].append(x0[4])
+        t2["iterations"].append(niter)
+        t2["xmin_1"].append(x_min[0])
+        t2["xmin_2"].append(x_min[1])
+        t2["xmin_3"].append(x_min[2])
+        t2["xmin_4"].append(x_min[3])
+        t2["xmin_5"].append(x_min[4])
+        t2["f(xmin)"].append(f(x_min))
+        t2["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[0] = old_x0_1
+    
+    for i in range(0, 101):
+        x2 = -50 + step * i
+        old_x0_2 = x0[1]
+        x0[1] = x2
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t2["x0_1"].append(x0[0])
+        t2["x0_2"].append(x0[1])
+        t2["x0_3"].append(x0[2])
+        t2["x0_4"].append(x0[3])
+        t2["x0_5"].append(x0[4])
+        t2["iterations"].append(niter)
+        t2["xmin_1"].append(x_min[0])
+        t2["xmin_2"].append(x_min[1])
+        t2["xmin_3"].append(x_min[2])
+        t2["xmin_4"].append(x_min[3])
+        t2["xmin_5"].append(x_min[4])
+        t2["f(xmin)"].append(f(x_min))
+        t2["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[1] = old_x0_2
+    
+    for i in range(0, 101):
+        x3 = -50 + step * i
+        old_x0_3 = x0[2]
+        x0[2] = x3
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t2["x0_1"].append(x0[0])
+        t2["x0_2"].append(x0[1])
+        t2["x0_3"].append(x0[2])
+        t2["x0_4"].append(x0[3])
+        t2["x0_5"].append(x0[4])
+        t2["iterations"].append(niter)
+        t2["xmin_1"].append(x_min[0])
+        t2["xmin_2"].append(x_min[1])
+        t2["xmin_3"].append(x_min[2])
+        t2["xmin_4"].append(x_min[3])
+        t2["xmin_5"].append(x_min[4])
+        t2["f(xmin)"].append(f(x_min))
+        t2["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[2] = old_x0_3
+    
+    for i in range(0, 101):
+        x4 = -50 + step * i
+        old_x0_4 = x0[3]
+        x0[3] = x4
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t2["x0_1"].append(x0[0])
+        t2["x0_2"].append(x0[1])
+        t2["x0_3"].append(x0[2])
+        t2["x0_4"].append(x0[3])
+        t2["x0_5"].append(x0[4])
+        t2["iterations"].append(niter)
+        t2["xmin_1"].append(x_min[0])
+        t2["xmin_2"].append(x_min[1])
+        t2["xmin_3"].append(x_min[2])
+        t2["xmin_4"].append(x_min[3])
+        t2["xmin_5"].append(x_min[4])
+        t2["f(xmin)"].append(f(x_min))
+        t2["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[3] = old_x0_4
+    for i in range(0, 101):
+        x5 = -50 + step * i
+        old_x0_5 = x0[4]
+        x0[4] = x5
+        x_min, niter = linear_minimize(
+            f, x0, [g1],
+            maxstep=10,
+            step_decrement=0.99,
+            eps=curr_eps
+        )
+        t2["x0_1"].append(x0[0])
+        t2["x0_2"].append(x0[1])
+        t2["x0_3"].append(x0[2])
+        t2["x0_4"].append(x0[3])
+        t2["x0_5"].append(x0[4])
+        t2["iterations"].append(niter)
+        t2["xmin_1"].append(x_min[0])
+        t2["xmin_2"].append(x_min[1])
+        t2["xmin_3"].append(x_min[2])
+        t2["xmin_4"].append(x_min[3])
+        t2["xmin_5"].append(x_min[4])
+        t2["f(xmin)"].append(f(x_min))
+        t2["||x0-xmin||"].append(np.linalg.norm(x_min - x0))
+        x0[4] = old_x0_5
+    table2 = pd.DataFrame(t2)
+    table2.to_excel('./table2.xlsx')
